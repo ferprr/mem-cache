752746778992b71cef7e20313c3ed61bf61fde4f
guilhermegbm <guilhermebmendonca60@gmail.com>
2021-03-29 16:20:27 -0300

    Leitura e tratamento básico da entrada finalizados

--- mem.cpp
+++ mem.cpp
@@ -1,10 +1,81 @@
 #include <iostream>
-#include <string.h>
+#include <string>
+#include <sstream>
 
-int main() {
+using namespace std;
 
-    int n, op;
-    string data;
+string *separaStringPorEspaco(int tamanho, string str)
+{
+    istringstream iss(str);
+
+    string *strVet = new string[tamanho];
+
+    int indexTotal = 0;
+    do
+    {
+        string subStr;
+        iss >> subStr;
+
+        strVet[indexTotal] = subStr;
+        indexTotal++;
+
+        if (indexTotal == tamanho)
+            break;
+    } while (iss);
+
+    return strVet;
+}
+
+int stringToInt(string s)
+{
+    stringstream strStream(s);
+    int strAsInt;
+    strStream >> strAsInt;
+    return strAsInt;
+}
+
+void executarComInputsSobDemanda()
+{
+    string linha;
+    //Memoria memoria;
+
+    while (getline(cin, linha))
+    {
+        if (linha.empty())
+        {
+            break;
+        }
+
+        string *vetEntrada = separaStringPorEspaco(3, linha);
+
+        string operacao = vetEntrada[1];
+
+        if (operacao == "0")
+        {
+            int enderecoMemoriaParaLeitura = stringToInt(vetEntrada[0]);
+
+            //cout << "Leitura: " << enderecoMemoriaParaLeitura << endl;
+
+            //TODO:
+            //string dadoLido = memoria.ler(enderecoMemoriaParaLeitura);
+        }
+        else if (operacao == "1")
+        {
+            int enderecoMemoriaParaEscrita = stringToInt(vetEntrada[0]);
+            string dadoEscrita = vetEntrada[2];
+
+            //cout << "Escrita: " << enderecoMemoriaParaEscrita << " --> " << dadoEscrita << endl;
+
+            //TODO:
+            //bool hitOuMiss = memoria.escreverNaCache(enderecoMemoriaParaEscrita);
+        }
+    }
+}
+
+int main()
+{
+
+    executarComInputsSobDemanda();
 
     return 0;
 }
